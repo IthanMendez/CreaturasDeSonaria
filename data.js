cde5b6ab235cdec8b8c45f78b64a0213582c7b71
IthanMendez <60452477+IthanMendez@users.noreply.github.com>
2025-07-26 23:44:13 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,91 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-defaul",
+      "name": "DEFAUL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.9988646848066196,
+          "pitch": 0.16828214653969908,
+          "title": "Montañas",
+          "text": "Sitio con gran altura, lagos congelados y nieve que cae, diversas cuevas en varias partes de la montaña"
+        },
+        {
+          "yaw": -1.3383734622334522,
+          "pitch": 1.0119190238243796,
+          "title": "Centrales",
+          "text": "Como su nombre lo indica, es el centro del mapa, aquí hay una gran cascada, varias cuevas y un lago en el que vienen criaturas de todas los biomas, de todo tipo, acuáticas, voladoras y terrestres."
+        },
+        {
+          "yaw": 1.586126782103971,
+          "pitch": 0.14103507133490467,
+          "title": "Islas flotantes",
+          "text": "Las criaturas voladoras suelen venir aquí a descansar o después de una pelea para recuperarse"
+        },
+        {
+          "yaw": 0.23219564839713946,
+          "pitch": -0.10250297382265039,
+          "title": "Islas flotantes",
+          "text": "Islas que estan medio a tres biomas, centrales, cueva de flores y volcán, hasta arriba hay un santuario de un guardian del juego"
+        },
+        {
+          "yaw": 0.8156806793491462,
+          "pitch": 0.310566122267538,
+          "title": "Cueva de flores",
+          "text": "Lugar ideal para anidar nuevas criaturas, mucha vegetación y comida abundante para los hervíboros"
+        },
+        {
+          "yaw": -0.17716473446151326,
+          "pitch": 0.18904885877428157,
+          "title": "Volcán",
+          "text": "Hace erupción cada cierto tiempo, tiene una cueva llena de lava, un lago y caminos que conectan a otros biomas."
+        },
+        {
+          "yaw": 2.2790571456543773,
+          "pitch": 0.241897826677393,
+          "title": "Desierto",
+          "text": "Lugar muy popular con poca vegetación, cuevas y un oasis con cascada en medio de este."
+        },
+        {
+          "yaw": -1.7802345261112187,
+          "pitch": 0.1317598519501555,
+          "title": "Inicio de Pantano",
+          "text": "Sitio no muy concurrido donde hay demasiada agua para pasar con facilidad con criaturas terrestres, árboles, vegetación exótica."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
